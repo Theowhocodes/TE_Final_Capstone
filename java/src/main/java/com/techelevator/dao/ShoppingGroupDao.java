cf21ec850921250ae7c16d8d964cd79d9ef50bd9
Jennifer Glick <jenn.chaney@gmail.com>
2022-12-06 10:56:49 -0500

    working on shopping group DAOs

--- java/src/main/java/com/techelevator/dao/ShoppingGroupDao.java
+++ java/src/main/java/com/techelevator/dao/ShoppingGroupDao.java
@@ -1,9 +1,17 @@
 package com.techelevator.dao;
 
+import com.techelevator.model.ShoppingGroup;
+
+import java.util.List;
+
 public interface ShoppingGroupDao {
     // SHOW ALL GROUPS A USER BELONGS TO
 
+    List<ShoppingGroup> getAllShoppingGroups(int userId);
+
+
     // CREATE A GROUP
+
 
     // JOIN A GROUP
 
