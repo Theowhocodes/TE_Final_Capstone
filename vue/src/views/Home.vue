4e1c5678940ce8d6ca35f63eab1f482fbc32049d
Aileen Hall <aghall5@gmail.com>
2022-12-07 10:18:24 -0500

    fixed?

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,12 +1,32 @@
 <template>
-  <div class="home">
+  <div class="home" >
     <h1>Home</h1>
-    <p>You must be authenticated to see this</p>
+    <the-board/>
   </div>
 </template>
 
 <script>
+import TheBoard from '../components/TheBoard.vue';
+
+
+
 export default {
+  components: {TheBoard  },
   name: "home"
 };
 </script>
+
+<style scoped>
+#groupBoard{
+  height:100vh ;
+  width: 100vmax;
+  background-size: 100% ;
+  background-repeat: no-repeat;
+  background-size:contain ;
+  margin: 0;
+  padding: 0;
+  border: 0;
+}
+
+
+</style>
