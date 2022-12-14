d58778c92a8016c770f5ce9c86dcef3d0f9b9f3b
Aileen Hall <aghall5@gmail.com>
2022-12-14 10:30:56 -0500

    table formatting

--- vue/src/components/GroupDetail.vue
+++ vue/src/components/GroupDetail.vue
@@ -1,63 +1,84 @@
 <template>
-  <div class="has-mw-md has-mw-xl-widescreen mx-auto py-12 px-8-mobile px-14-tablet has-background-white">
-    
-      <h3> {{group.groupName}} | Invitation Code: {{group.invitationCode}}</h3>
-      <h4>Your Group Lists</h4>
-      <div class= "groupList"
-      v-for="list in lists"
-      v-bind:key="list.groupId"
-      v-bind:list="list"
-      >
-      
-      List name: <router-link v-bind:to="{ name: 'list', params: { listId: list.listId } }">
-          {{list.listName}} </router-link> | Total items on list: {{list.count}} | List status: {{ list.claimed ? "Claimed" : "Unclaimed" }}
-    
-    
-      </div>
+  <div
+    class="
+      has-mw-md has-mw-xl-widescreen
+      mx-auto
+      py-12
+      px-8-mobile px-14-tablet
+      has-background-white
+    "
+  >
+    <h3>{{ group.groupName }} | Invitation Code: {{ group.invitationCode }}</h3>
+
+    <h4>Your Lists</h4>
+    <br />
+    <table>
+      <thead>
+        <tr>
+          <th>List Name</th>
+          
+          <th>Total Items on List</th>
+          
+          <th>List Status</th>
+          
+        </tr>
+      </thead>
+      <br />
+
+      <tbody>
+        <tr
+          class="groupList"
+          v-for="list in lists"
+          v-bind:key="list.groupId"
+          v-bind:list="list"
+        >
+          <td>
+            <router-link
+              v-bind:to="{ name: 'list', params: { listId: list.listId } }"
+            >
+              {{ list.listName }}
+            </router-link>
+          </td>
+          <td>{{ list.count }}</td>
+          <td>{{ list.claimed ? "Claimed" : "Unclaimed" }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
-<script> 
-
-import groupService from '../services/GroupService.js';
+<script>
+import groupService from "../services/GroupService.js";
 
 export default {
   name: "group-detail",
 
   data() {
     return {
-    lists: [],
-    list: {
+      lists: [],
+      list: {},
 
-    },
-
-    group: {
-
-    }
+      group: {},
     };
   },
 
   created() {
-    const groupId = this.$route.params.groupId; 
+    const groupId = this.$route.params.groupId;
 
-    groupService.getAllListsByGroupId(groupId).then(response => {
-      console.log(response)
-    this.lists = response.data; 
-      
+    groupService.getAllListsByGroupId(groupId).then((response) => {
+      console.log(response);
+      this.lists = response.data;
     });
 
-    groupService.getOneGroupById(groupId).then(response => {
+    groupService.getOneGroupById(groupId).then((response) => {
       this.group = response.data;
-    }
-    )
-  }
-
-  
-
-}
+    });
+  },
+};
 </script>
 
-<style> /*
+<style>
+/*
 #groupDetails{
  margin: auto;
  overflow: auto;
