5495e8f9f058c32fd5aa718a9179c0aadfd7b6f7
Jennifer Glick <jenn.chaney@gmail.com>
2022-12-13 22:12:19 -0500

    trying stuff with Bulma tile layouts

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app" >
-    <div id="nav">
+    <!--<div id="nav">
       <router-link v-bind:to="{ name: 'home' }">Home </router-link>
       <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">| Logout</router-link>
-    </div>
+    </div> -->
     <router-view />
   </div>
 </template>
@@ -14,7 +14,7 @@
 @import "~bulma/css/bulma.css";
 
 #app{
-  background-image: url("https://images.pexels.com/photos/733852/pexels-photo-733852.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"); 
+  /*background-image: url("https://images.pexels.com/photos/733852/pexels-photo-733852.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"); */
   height:100vh ;
   width: 100vmax;
   background-size: 100% ;
